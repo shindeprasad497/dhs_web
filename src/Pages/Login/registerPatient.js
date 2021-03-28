c0ff2b21d27729b4460a6e4470e6f3236b1b6655
Neha Sharma <neha.sharma@neebal.com>
2021-03-28 17:53:34 +0530

    Register patient changes

--- src/Pages/Login/registerPatient.js
+++ src/Pages/Login/registerPatient.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import API_KEY from "../../Api/api";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dropdown from 'react-overlays/Dropdown';
+import Data from "../../MockData/BloodGroups.json";
 import {
   Card,
   Button,
@@ -10,43 +13,87 @@ import {
   FormControl,
 } from "react-bootstrap";
 export default function PatientRegistration({ history }) {
-    const [name, setName] = useState(null);
-    const [email, setEmail] = useState(null);
-    const [contact, setContact] = useState(null);
+    const [firstName, setFirstName] = useState(null);
+    const [lastName, setLastName] = useState(null);
+    const [email, setEmailId] = useState(null);
+    const [contactNo, setContactNo] = useState(null);
     const [address, setAddress] = useState(null);
+    const [bloodgroup, setBloodGroup] = useState(null);
+    const [bloodgroups, setBloodGroups] = useState(Data);
+    const [aadharNo, setAadharNo] = useState(null);    
+    const [password, setPassword] = useState(null);    
   
     const handleSave = () => {
       const values = {
-        hospitalName: name,
+        firstName: firstName,
+        lastName: lastName,
         emailId: email,
-        hospitalContactNo: contact,
-        hospitaladdress: address,
+        contactNo: contactNo,
+        address: address,
+        bloodGroup: bloodgroup,
+        addharNo: aadharNo,
+        password: password
       };
       console.log(values,"values")
       axios
         .post(`${API_KEY.URL.baseurl}/${API_KEY.path.hospitalSave}`, values)
         .then((res) => {
           console.log(res);
+
           history.push(`hospital-view/${res?.data?.id}`)
         })
         .catch((err) => console.log(err));
     };
+
+     useEffect(() => {
+     setBloodGroups(Data);
+     console.log(bloodgroups);
+  }, [])
     return (
       <div>
         <Card>
-          <Card.Header as="h5">Hospital Create</Card.Header>
+          <Card.Header as="h5">Patient Registration Form</Card.Header>
           <Card.Body>
+            <Card.Title className="text-center">Patient Information</Card.Title>
             <InputGroup className="mb-3">
               <InputGroup.Prepend style={{ width: "40%" }}>
                 <InputGroup.Text id="basic-addon1">
-                  Enter Hopital Name
+                  Enter First Name
                 </InputGroup.Text>
               </InputGroup.Prepend>
               <FormControl
-                placeholder="Name"
-                aria-label="Name"
+                placeholder="First Name"
+                aria-label="firstName"
                 aria-describedby="basic-addon1"
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend style={{ width: "40%" }}>
+                <InputGroup.Text id="basic-addon1">
+                  Enter Last Name
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Last Name"
+                aria-label="lastName"
+                aria-describedby="basic-addon1"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend style={{ width: "40%" }}>
+                <InputGroup.Text id="basic-addon1">
+                  Enter Contact No
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Contact No"
+                aria-label="contactNo"
+                aria-describedby="basic-addon1"
+                onChange={(e) => setContactNo(e.target.value)}
               />
             </InputGroup>
   
@@ -56,36 +103,75 @@ export default function PatientRegistration({ history }) {
               </InputGroup.Prepend>
               <FormControl
                 placeholder="Email"
-                aria-label="Email"
+                aria-label="email"
                 aria-describedby="basic-addon1"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => setEmailId(e.target.value)}
               />
             </InputGroup>
-  
+
             <InputGroup className="mb-3">
               <InputGroup.Prepend style={{ width: "40%" }}>
                 <InputGroup.Text id="basic-addon1">
-                  Enter Contact No
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                placeholder="Contact No"
-                aria-label="Contact No"
-                aria-describedby="basic-addon1"
-                onChange={(e) => setContact(e.target.value)}
-              />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Prepend style={{ width: "40%" }}>
-                <InputGroup.Text id="basic-addon1">
-                  Enter Hopital Address
+                  Enter Address
                 </InputGroup.Text>
               </InputGroup.Prepend>
               <FormControl
                 placeholder="Address"
-                aria-label="Address"
+                aria-label="address"
                 aria-describedby="basic-addon1"
                 onChange={(e) => setAddress(e.target.value)}
+              />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend style={{ width: "40%" }}>
+                <InputGroup.Text id="basic-addon1">
+                  Select Blood Group
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+
+              <FormControl
+              as="select"
+              custom
+              placeholder="Select Blood Group"
+              aria-label="bloodgroup"
+              aria-describedby="bloodgroup"
+              onChange={(e) => setBloodGroup(e.target.value)}
+              value={bloodgroup}
+            >
+                {bloodgroups.map(hos=> <option key={hos?.name}  value={hos?.name}>{hos?.name}</option>
+                )}
+             
+             
+            </FormControl>
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend style={{ width: "40%" }}>
+                <InputGroup.Text id="basic-addon1">
+                  Enter Aadhar No
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Aadhar No"
+                aria-label="aadharNo"
+                aria-describedby="basic-addon1"
+                onChange={(e) => setAadharNo(e.target.value)}
+              />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend style={{ width: "40%" }}>
+                <InputGroup.Text id="basic-addon1">
+                  Enter Password
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Password"
+                aria-label="password"
+                type="password"
+                aria-describedby="basic-addon1"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </InputGroup>
   
